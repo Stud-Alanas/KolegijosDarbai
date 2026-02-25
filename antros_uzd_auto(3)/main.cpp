50171943a0dd658eefcf29c36f34387a2459bbc8
BattleMoose666 <alanas.j.kireilis@gmail.com>
2026-02-25 10:19:15 +0200

    trecio trecias

---
+++ antros_uzd_auto(3)/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string Make = "Volvo";
+    string Modelis = "C30";
+    int pagaminta = 2006;
+    string litrazas = "1.6";
+    string spalva = "Pilkos spavlos";
+
+    cout << "Automobilis - " << Make << " " << Modelis << " yra pagamintas " << pagaminta << " Metais. Jo motoras " << litrazas << " litrazo. Automobilis yra " << spalva << endl;
+
+    return 0;
+}
