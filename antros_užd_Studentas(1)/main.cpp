b7583f004a9afd19cddffadd7a79b52e0b44e18e
BattleMoose666 <alanas.j.kireilis@gmail.com>
2026-02-25 09:36:59 +0200

    antros pirmas

---
+++ antros_užd_Studentas(1)/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main() {
+    string vardas = "Alanas";
+    string pavarde = "Kireilis";
+    int amzius = 27;
+    string grupe = "IS25SN";
+    int kursas = 1;
+    string studijuPrograma = "Informacines sistemos ir sauga";
+
+    cout << "Studentas: " << vardas << " " << pavarde << ", amzius: " << amzius << ", grupe - " << grupe << ", kursas - " << kursas << " , programa: " << studijuPrograma << endl;
+
+    return 0;
+
+}
