bd39d7825637eb0afde6e5c940bac1acc8c632ac
BattleMoose666 <alanas.j.kireilis@gmail.com>
2026-02-25 09:47:38 +0200

    antro antras

---
+++ antros_uzd_Sportoklubas(2)/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string klubas = "Pasvalio pieno zvaigdzes";
+    int ikurimoMetai = 1999;
+    string savininkas = "Eugenijus Budreika";
+    string arena = "Pasvalio pieno zvaigdziu arena";
+    int vietuSkaicius = 1200;
+
+    cout << "Krepsinio klubas: " << klubas << " Ikurtas: " << ikurimoMetai << " Klubo savininkas: " << savininkas << "Arena: " << arena << "Arenos vietu skaicius: " <<vietuSkaicius << endl;
+
+    return 0;
+}
