c7cb388f94848734e8f8e321608b41a763dae6fa
BattleMoose666 <alanas.j.kireilis@gmail.com>
2026-03-14 09:57:02 +0200

    Aprasiau funkcija mokiniams ir pazymiui

--- praktiniai darbai/antras praktinis darbas/main.cpp
+++ praktiniai darbai/antras praktinis darbas/main.cpp
@@ -34,8 +34,93 @@ void pridetiMokini(string vardai[], int pazymiai[][MAX_PAZYMIU], int pazymiuKiek
     mokiniuKiekis++;
     cout << "Mokinys sekmingai pridetas.\n";
 }
+// Funkcija visiems mokiniams parodyti
+void rodytiVisus(string vardai[], int pazymiai[][MAX_PAZYMIU], int pazymiuKiekis[], int mokiniuKiekis) {
+    if (mokiniuKiekis == 0) {
+        cout << "Sarasas tuscias.\n";
+        return;
+    }
+
+    cout << "\n--- Visi mokiniai ir ju pazymiai ---\n";
+    for (int i = 0; i < mokiniuKiekis; i++) {
+        cout << i + 1 << ". " << vardai[i] << " -> ";
+        for (int j = 0; j < pazymiuKiekis[i]; j++) {
+            cout << pazymiai[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+// Funkcija vieno mokinio informacijos parodymui
+void rodytiViena(string vardai[], int pazymiai[][MAX_PAZYMIU], int pazymiuKiekis[], int mokiniuKiekis) {
+    if (mokiniuKiekis == 0) {
+        cout << "Sarasas tuscias.\n";
+        return;
+    }
+
+    int nr;
+    cout << "Iveskite mokinio numeri (1-" << mokiniuKiekis << "): ";
+    cin >> nr;
+
+    if (nr < 1 || nr > mokiniuKiekis) {
+        cout << "Tokio mokinio nera.\n";
+        return;
+    }
+
+    nr--;
+
+    cout << "\nMokinys: " << vardai[nr] << endl;
+    cout << "Pazymiai: ";
+    for (int i = 0; i < pazymiuKiekis[nr]; i++) {
+        cout << pazymiai[nr][i] << " ";
+    }
+    cout << endl;
+}
+
+// Funkcija pažymiui atnaujinti
+void atnaujintiPazymi(string vardai[], int pazymiai[][MAX_PAZYMIU], int pazymiuKiekis[], int mokiniuKiekis) {
+    if (mokiniuKiekis == 0) {
+        cout << "Sarasas tuscias.\n";
+        return;
+    }
+
+    int mokNr;
+    cout << "Iveskite mokinio numeri (1-" << mokiniuKiekis << "): ";
+    cin >> mokNr;
+
+    if (mokNr < 1 || mokNr > mokiniuKiekis) {
+        cout << "Tokio mokinio nera.\n";
+        return;
+    }
+
+    mokNr--;
+
+    cout << "Mokinys: " << vardai[mokNr] << endl;
+    cout << "Esami pazymiai:\n";
+    for (int i = 0; i < pazymiuKiekis[mokNr]; i++) {
+        cout << i + 1 << ". " << pazymiai[mokNr][i] << endl;
+    }
+
+    int pazNr;
+    cout << "Kuri pazymi norite pakeisti? (1-" << pazymiuKiekis[mokNr] << "): ";
+    cin >> pazNr;
+
+    if (pazNr < 1 || pazNr > pazymiuKiekis[mokNr]) {
+        cout << "Tokio pazymio nera.\n";
+        return;
+    }
+
+    int naujas;
+    do {
+        cout << "Iveskite nauja pazymi (1-10): ";
+        cin >> naujas;
+    } while (naujas < 1 || naujas > 10);
+
+    pazymiai[mokNr][pazNr - 1] = naujas;
+    cout << "Pazymys sekmingai atnaujintas.\n";
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
     return 0;
 }
