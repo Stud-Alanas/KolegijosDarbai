06a9da84be16fa5a6588f2be79ff23ae2443f05b
BattleMoose666 <alanas.j.kireilis@gmail.com>
2026-05-04 09:04:47 +0300

    Pasidarem faila su textiniu doku ir apsirasiau bazines konstantas

---
+++ praktiniai darbai/untitled1/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+const int MAX_MENU = 50;
+const double PVM = 0.21;
+
+struct menuItemType {
+    string menuItem;
+    double menuPrice;
+};
+
+int main() {
+
+}
